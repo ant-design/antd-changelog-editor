70de42d2d14f12416eb31b72059ecf75a24826d6
afc163 <afc163@gmail.com>
2020-08-10 18:23:05 +0800

    fix umi test

--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -1,9 +1,23 @@
-import 'jest';
 import Index from '..';
 import React from 'react';
 import renderer, { ReactTestInstance, ReactTestRenderer } from 'react-test-renderer';
 
 describe('Page: index', () => {
+  beforeEach(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
   it('Render correctly', () => {
     const wrapper: ReactTestRenderer = renderer.create(<Index />);
     expect(wrapper.root.children.length).toBe(1);
